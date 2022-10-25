691d2576e7ee9b5014a3ad7f0d43c82161d71258
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-10-25 14:03:51 +0500

    Initial commit

---
+++ src/main/java/com/example/clanstesttask/ClansTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.clanstesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClansTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClansTestTaskApplication.class, args);
+    }
+
+}
