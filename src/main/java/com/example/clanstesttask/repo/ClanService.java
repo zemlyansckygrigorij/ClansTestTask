25c3935844dedd2397c41e60df5f9e974f447f11
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-10-27 15:57:08 +0500

    update interface ClanService add methods Clan getClan(long clanId);                                              List<Clan> getAllClan();                                              boolean  updateGold(long clanId, int gold) throws Exception;

--- src/main/java/com/example/clanstesttask/repo/ClanService.java
+++ src/main/java/com/example/clanstesttask/repo/ClanService.java
@@ -1,7 +1,11 @@
 package com.example.clanstesttask.repo;
 
 import com.example.clanstesttask.entity.Clan;
+import java.util.List;
 
 public interface ClanService {
-    Clan get(long clanId);
+    Clan getClan(long clanId);
+    List<Clan> getAllClan();
+    boolean  updateGold(long clanId, int gold) throws Exception;
+    Clan createClan(Clan clan);
 }
