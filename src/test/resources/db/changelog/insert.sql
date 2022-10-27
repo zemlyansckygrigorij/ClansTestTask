b0cf1c576f5f52f8cfd5ee2a2f659bdcf4fbd885
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-10-27 15:54:16 +0500

    create file insert.sql for filling tables in database for testing

---
+++ src/test/resources/db/changelog/insert.sql
@@ -0,0 +1,38 @@
+
+delete from clans;
+insert into clans(name, gold) values('Crimson Tide', 0);
+insert into clans(name, gold) values('Amaranth Legion', 0);
+insert into clans(name, gold) values('Emerald Fury', 0);
+insert into clans(name, gold) values('Scarlet Agony', 0);
+insert into clans(name, gold) values('Black Lotus', 0);
+insert into clans(name, gold) values('Golden Axe', 0);
+
+
+delete from users;
+insert into users(name, gold) values('vinsent', 4000);
+insert into users(name, gold) values('мэйн', 4000);
+insert into users(name, gold) values('Кëрт', 4000);
+insert into users(name, gold) values('крис', 4000);
+insert into users(name, gold) values('мэйт', 4000);
+insert into users(name, gold) values('Вилл', 4000);
+insert into users(name, gold) values('виен', 4000);
+insert into users(name, gold) values('Лиам', 4000);
+insert into users(name, gold) values('Mina', 4000);
+insert into users(name, gold) values('чон', 4000);
+insert into users(name, gold) values('Vanessa', 4000);
+insert into users(name, gold) values('𝙹𝚒𝚖𝚖𝚢', 4000);
+insert into users(name, gold) values('Taehyung', 4000);
+insert into users(name, gold) values('Лили', 4000);
+insert into users(name, gold) values('Айли', 4000);
+insert into users(name, gold) values('Винс', 4000);
+insert into users(name, gold) values('Даниэль', 4000);
+insert into users(name, gold) values('Luna', 4000);
+insert into users(name, gold) values('кенни', 4000);
+insert into users(name, gold) values('лэй', 4000);
+insert into users(name, gold) values('Ян', 4000);
+insert into users(name, gold) values('Мина', 4000);
+insert into users(name, gold) values('диор', 4000);
+insert into users(name, gold) values('Чану', 4000);
+insert into users(name, gold) values('Джен', 4000);
+insert into users(name, gold) values('Рэй',4000);
+insert into users(name, gold) values('jasper', 4000);
