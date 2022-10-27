29a84dfabe628a692dafeeb187d90e48cea7a34b
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-10-27 15:36:43 +0500

    create class UserRowMapper for make objects User from data table users

---
+++ src/main/java/com/example/clanstesttask/entity/UserRowMapper.java
@@ -0,0 +1,18 @@
+package com.example.clanstesttask.entity;
+
+import org.springframework.jdbc.core.RowMapper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserRowMapper implements RowMapper<User> {
+
+    @Override
+    public User mapRow(ResultSet resultSet, int i) throws SQLException {
+        User user = new User();
+        user.setId(resultSet.getLong("id"));
+        user.setName(resultSet.getString("name"));
+        user.setGold(resultSet.getInt("gold"));
+
+        return user;
+    }
+}
