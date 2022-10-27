f44d2bc0c7c34b0ef236a207f191cc7f0adcef19
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-10-27 15:39:47 +0500

    create class SpringJdbcConfig for get bean DataSource

---
+++ src/main/java/com/example/clanstesttask/config/SpringJdbcConfig.java
@@ -0,0 +1,34 @@
+package com.example.clanstesttask.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("com.example.clanstesttask")
+public class SpringJdbcConfig {
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername( username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+}
+
